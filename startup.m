675733d0aa6ba699f36543ce270f51e18b8244a7
Manjari Narayan (VA) <manjari.narayan@gmail.com>
2016-02-19 17:56:57 -0800

    Added path setup in startup.m

---
+++ startup.m
@@ -0,0 +1,8 @@
+% Network Metrics: Either BCT or matlab-bgl or custom
+
+METRICS_PATH= '../packages/BCT'
+addpath(genpath(METRICS_PATH));
+
+% visualization
+PLOTLY_PATH = '../packages/MATLAB-api'
+addpath(genpath(PLOTLY_PATH));
