c8dd426ce16a46b49b610cee2217e3db26747862
Manjari Narayan <manjari.narayan@gmail.com>
2016-02-29 09:04:29 -0800

    Added checks for isWeighted flag. Increased threshold range

--- Step02_choose_metrics.m
+++ Step02_choose_metrics.m
@@ -2,15 +2,23 @@
 %%%%% Pick Metrics %%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%
 bct_funs = {@betweenness_bin, ... %1
-									@clustering_coef_bu, ... 	%2
-									@efficiency_bin, ...
-									@eigenvector_centrality_und, ...
-									@betweenness_wei, ...
-									@clustering_coef_wu, ...
-									@efficiency_wei, ...
-									 };
-bct_num = 7;
-isWeighted = 0; % Eventually choose this and network metrics with error checking
+			@clustering_coef_bu, ... 	%2
+			@efficiency_bin, ...
+			@eigenvector_centrality_und, ...
+			@betweenness_wei, ...
+			@clustering_coef_wu, ...
+			@efficiency_wei, ...
+			 };
+bct_num = 4;
+isWeighted = 1; % Eventually choose this and network metrics with error checking
+if(strfind(func2str(bct_funs{bct_num}),'wei')|strfind(func2str(bct_funs{bct_num}),'wu'))
+	isWeighted =1;
+elseif(isWeighted)
+	warning('isWeighted setting likely incorrect')
+	if(strfind(func2str(bct_funs{bct_num}),'bin')|strfind(func2str(bct_funs{bct_num}),'bu'))
+		isWeighted=0;
+	end
+end
 disp(['Metric Chosen is ' func2str(bct_funs{bct_num}) ', isWeighted: ' num2str(isWeighted)])
 
 
@@ -21,8 +29,6 @@ p = size(Sigma,1);
 node = floor(p/2); % Specity node/region of your choice
 t_idx = 5; % Choose from 1 to 5 where 1 corresponds to few observations at t=2p, and 5 corresponds to t=10p
 
-
-
 %%%%%%%%%%%%%%%%%%
 %%%Choose Simulation Parameters%%%
 %%%%%%%%%%%%%%%%%%%
@@ -32,6 +38,6 @@ n_trials = 50; % Monte carlo trials
 %%%%%%%%%%%%%%%%%%%
 n_thresh = 25; % Granularity of shrinkage or thresholding parameter
 tau_start = .1;
-tau_stop = .2;
+tau_stop = .9;
 taus = linspace(tau_start,tau_stop,n_thresh);
 %%%%%%%%%%%%%%%%%%%
