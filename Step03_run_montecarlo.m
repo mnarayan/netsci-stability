e8258ef3e4572a3400a8be41fa87e8f62fa8c370
Manjari Narayan <manjari.narayan@gmail.com>
2016-02-29 09:05:48 -0800

    Fixed complex value bugs for weighted and binary analysis

--- Step03_run_montecarlo.m
+++ Step03_run_montecarlo.m
@@ -29,20 +29,25 @@ if(isWeighted)
 	for trial=1:n_trials
 		trial
 		for t=1:length(obs)
-			X = randn(obs(t),p)*sqrt(D)*V';
+			X = randn(obs(t),p)*V*sqrt(D)*V';
 			Sighat = cov(X); Sighat(find(eye(p))) = 0;
 			for tau=1:length(taus)
 				%soft(g, τ ) := sign(g) · (|g| − τ )+
-				softSig = (abs(Sighat)-taus(tau));
+				softSig = triu((abs(Sighat)-taus(tau)),1);
 				threshSig = softSig.*(softSig>=0) + 0.*(softSig<0);
 				softthreshSig = sign(Sighat).*threshSig;
+				softthreshSig = softthreshSig + softthreshSig';
+				assert(sum(diag(softthreshSig)<=0)~=0,'Negative values on diagonal');
+				%[V2 D2] = eig(softthreshSig); softthreshSig = V2*(D2+eye(p)*(.01+abs(min(diag(D2)))))*V2';
 				switch func2str(bct_funs{bct_num})
-				case 'efficiency_bin'
-					tmp_stats = feval(bct_funs{bct_num},softthreshSig,1);
 				case 'efficiency_wei'
 					tmp_stats = feval(bct_funs{bct_num},softthreshSig,1);
 				otherwise
 					tmp_stats = feval(bct_funs{bct_num},softthreshSig);
+				end
+				if(~isreal(tmp_stats))
+					warning('Network metrics are complex. Taking absolute values'); 
+					tmp_stats = abs(tmp_stats);
 				end
 				netstat_nodes(trial,t,:,tau) = tmp_stats;
 			end
@@ -52,15 +57,27 @@ else
 	for trial=1:n_trials
 		trial
 		for t=1:length(obs)
-			X = randn(obs(t),p)*sqrt(D)*V';
+			X = randn(obs(t),p)*V*sqrt(D)*V';
 			Sighat = cov(X); Sighat(find(eye(p))) = 0;
+			assert(sum(diag(Sighat)<=0)~=0,'Negative values on diagonal');			
 			for tau=1:length(taus)
-				tmp_stats = feval(bct_funs{bct_num},1*(abs(Sighat)>taus(tau)));
+				switch func2str(bct_funs{bct_num})
+				case 'efficiency_bin'
+					tmp_stats = feval(bct_funs{bct_num},1*(abs(Sighat)>taus(tau)),1);
+				otherwise
+					tmp_stats = feval(bct_funs{bct_num},1*(abs(Sighat)>taus(tau)));
+				end
+				if(~isreal(tmp_stats))
+					warning('Network metrics are complex. Taking absolute values'); 
+					tmp_stats = abs(tmp_stats);
+				end
 				netstat_nodes(trial,t,:,tau) = tmp_stats;
 			end
 		end
 	end
 end
+
+save('tmp_netstat_nodes','netstat_nodes');
 
 % Plot all nodes
 % for time=1:length(obs)
