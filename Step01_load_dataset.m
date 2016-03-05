2364e643382af8a48fe9f36b80083dc464b41070
Manjari Narayan <manjari.narayan@gmail.com>
2016-03-05 15:47:08 -0800

    Generalized loading data process. Added more script options. Added parcor usage option

--- Step01_load_dataset.m
+++ Step01_load_dataset.m
@@ -1,5 +1,24 @@
 startup;
-tmp_dataset = load('Data/RestingMatrices.mat')
-Sigma = tmp_dataset.Sighat; % Specify your own pxp empirical covariance matrix or coherence matrix
-usePlotly = 1;
-graphtype = 'Resting_p18'
+
+datapath = 'Data'
+datafile = 'RestingMatrices.mat'
+fieldname = 'Sighat' % Variable containing an empirical correlation, covariance matrix;
+useParCor = 0; % Using standard correlation,covariance matrix
+
+try
+	help plotly;
+	usePlotly = 1; 	% Use plotly toolbox if available
+catch
+	usePlotly = 0;	% Plotly likely unavailable.
+end
+
+visible = 0;	% Plot figures or turn off plot checks.
+
+if(~useParCor) 
+	graphtype = 'Resting_p18'; % Name dataset/graph origin. 
+else
+	graphtype = 'Resting_p18_parcor'; % Name dataset/graph origin. 
+end
+
+tmp_dataset = load([datapath '/' datafile]);
+Sigma = getfield(tmp_dataset,fieldname); % Specify your own pxp empirical covariance matrix or coherence matrix
