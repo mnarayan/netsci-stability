193a12aed8ab0756360757bea93c94b209660e26
Manjari Narayan (VA) <manjari.narayan@gmail.com>
2016-04-19 15:45:22 -0700

    Added more test functions

--- netfun/HITS.m
+++ netfun/HITS.m
@@ -1,10 +1,10 @@
-function [hubs authorities eig_centrality] =  HITS(A,varargin); 
-	% HITS Algorithm 
+function [hubs authorities eig_centrality] =  HITS(A,varargin);
+	% HITS Algorithm
 	% (Kleinberg)
 	% Given a directed and thus asymmetric connectivity matrix A, HITS(A) returns centrality scores for hubs and authorities
 	% These are directed analogues of eigenvector centrality
-	% 
-	% 
+	%
+	%
 	% Test matrix
 	% A = zeros(4,4);
 	% A(1,2) = 1; A(1,3) = 1;
@@ -13,8 +13,8 @@ function [hubs authorities eig_centrality] =  HITS(A,varargin);
 	%
 	%
 	% Test matrix
-	% A = zeros(3,3); 
-	% A(1,3) = 1; A(2,3) = 1; 
+	% A = zeros(3,3);
+	% A(1,3) = 1; A(2,3) = 1;
 	% hubs = (2,2,0); authorities = (0,0,2);
 
 	if(nargin>=2)
@@ -24,13 +24,13 @@ function [hubs authorities eig_centrality] =  HITS(A,varargin);
 		else
 			visible = 0;
 		end
-		
+
 		if(isfield(opts,'bipartite'))
 			bipartite = opts.bipartite;
 		else
 			bipartite = 1;
 		end
-		
+
 		if(isfield(opts,'beta'))
 			beta = opts.beta;
 		else
@@ -41,42 +41,40 @@ function [hubs authorities eig_centrality] =  HITS(A,varargin);
 		bipartite = 1;
 		beta = 1;
 	end
-	
+
 	assert(sum(sum(abs(A-A')))>1,'Adjacency matrix is symmetric and undirected');
-	
+
 	L_in = beta.*A*A' + (1-beta)/size(A,1).*ones(size(A,1),size(A,1));
 	L_out = beta.*A'*A + (1-beta)/size(A,1).*ones(size(A,1),size(A,1));
-	
-	[V_in D_in] = eig(L_in); 	
+
+	[V_in D_in] = eig(L_in);
 	[sorted_D_in idx_in]  = sort(diag(D_in),'descend');
 	if(visible)
 		disp('Largest two eigenvalues of AA^T ')
-		sorted_D_in(1:2)'	 
+		sorted_D_in(1:2)'
 	end
 	hubs = V_in(:,idx_in(1)); % ordered from highest to lowest
 	hubs = hubs/sum(hubs);
 	assert(D_in(idx_in(1),idx_in(1))~=D_in(idx_in(2),idx_in(2)),'Principal eigenvalue is not unique. No stable hub/auth score');
-	
-	[V_out D_out] = eig(L_out); 	
-	[sorted_D_out idx_out]  = sort(diag(D_out),'descend'); 
+
+	[V_out D_out] = eig(L_out);
+	[sorted_D_out idx_out]  = sort(diag(D_out),'descend');
 	if(visible)
 		disp('Largest two eigenvalues of A^TA ')
 		sorted_D_out(1:2)'
 	end
 	authorities = V_out(:,idx_out(1)); % ordered from highest to lowest
 	authorities = authorities/sum(authorities);
-	
-	% Make symmetric, 
-	symA = (A+A'); 
+
+	% Make symmetric,
+	symA = (A+A');
 	diagA = inv(diag(sum(abs(symA))));
 	symA = diagA.^(.5)*symA*diagA.^(.5);
 	[V D] = eig(symA);
 	[sorted_D idx_und]  = sort(diag(D),'descend');
 	if(visible)
 		disp('Largest two eigenvalues of symmetricized A ')
-		sorted_D(1:2)'	 		
+		sorted_D(1:2)'
 	end
 	eig_centrality = V(:,idx_in(1)); % ordered from highest to lowest
 	eig_centrality = eig_centrality /sum(eig_centrality );
-
-	
--- netfun/test_HITS.m
+++ netfun/test_HITS.m
@@ -1,4 +1,4 @@
-beta = .999;
+beta = .99;
 
 % Test matrix
 A = zeros(3,3);
@@ -6,7 +6,6 @@ A(1,3) = 1; A(2,3) = 1;
 [hubs authorities eig_centrality] =  hits(A);
 [h_pgrank] = pagerank_centrality(A, beta); [a_pgrank] = pagerank_centrality(A',beta);
 centrality2centralization(eig_centrality,'eigenvector',1)
-
 assert(~any(hubs - [.5 .5 0]'),'Hubs are incorrect for [0 0 1; 0 1 0; 0 0 0]');
 assert(~any(authorities - [0 0 1]'),'Authorities are incorrect for [0 0 1; 0 1 0; 0 0 0]');
 clear;
@@ -17,21 +16,22 @@ clear;
 A = zeros(4,4);
 A(2,1) = 1; A(2,3) = 1; A(3,4) = 1;
 [hubs authorities eig_centrality] =  hits(A);
-[hubs authorities] =  rand_hits(A,1-beta);
-[h_pgrank] = pagerank_centrality(A, beta); [a_pgrank] = pagerank_centrality(A',beta);
+assert(~any(hubs - [0 1 0 0]'),'Hubs are incorrect');
+assert(sum(authorities - [.5 0 .5 0]')< 1e-3,'Authorities are incorrect ');
+[hubs authorities] =  rand_hits(A,struct('beta',1-beta));
+[h_pgrank] = pagerank(A, struct('beta',1-beta)); [a_pgrank] = pagerank(A',struct('beta',1-beta));
 centrality2centralization(hubs,'hub',1)
 centrality2centralization(authorities,'authority',1)
 centrality2centralization(h_pgrank,'pagerank',1)
 centrality2centralization(a_pgrank,'pagerank',1)
-assert(~any(hubs - [0 1 0 0]'),'Hubs are incorrect');
-assert(sum(authorities - [.5 0 .5 0]')< 1e-3,'Authorities are incorrect ');
-
 
 % Problematic Tests (Springer chapter)
 % p1 -> p_2; p1->p3; p2 -> p_3 -> p_4
 A = zeros(4,4);
 A(1,2) = 1; A(1,3) = 1;  A(2,3) = 1; A(3,4) = 1;
 [hubs authorities eig_centrality] =  hits(A);
+assert(~any(hubs - [.618 .382 0  0]'),'Hubs are incorrect for [0 1 1 0; 0 0 1 0; 0 0 0 1; 0 0 0 0]');
+assert(~any(authorities - [0 0 .618 .382]'),'Authorities are incorrect for [0 1 1 0; 0 0 1 0; 0 0 0 1; 0 0 0 0]');
 [hubs authorities] =  rand_hits(A,1-beta);
 [h_pgrank] = pagerank_centrality(A, beta); [a_pgrank] = pagerank_centrality(A',beta);
 
@@ -41,13 +41,13 @@ A(1,2) = 1; A(1,3) = 1;
 A(3,4) = 1; A(3,2) = 1;
 A(4,1) = 1;
 [hubs authorities eig_centrality] =  hits(A);
+assert(~any(hubs - [.5 0 .5 0]'),'Hubs are incorrect');
+assert(sum(authorities - [0 .5 .25 .25]')< 1e-3,'Authorities are incorrect ');
 [h_pgrank] = pagerank_centrality(A, beta); [a_pgrank] = pagerank_centrality(A',beta);
 centrality2centralization(hubs,'hub',1)
 centrality2centralization(authorities,'authority',1)
 centrality2centralization(h_pgrank,'pagerank',1)
 centrality2centralization(a_pgrank,'pagerank',1)
-assert(~any(hubs - [.5 0 .5 0]'),'Hubs are incorrect');
-assert(sum(authorities - [0 .5 .25 .25]')< 1e-3,'Authorities are incorrect ');
 clear;
 
 
@@ -58,60 +58,27 @@ A(3,1) = 1; A(3,3) = 0; A(3,4) = 2;
 A(4,4) = 0; A(4,5) = 1; A(5,7) = 1;
 A(6,6) = 0; A(6,7) = 1; A(7,4) = 2; A(7,5) = 1;, A(7,7) = 0;
 [hubs authorities eig_centrality] =  hits(A);
-[h_pgrank] = pagerank_centrality(A, beta); [a_pgrank] = pagerank_centrality(A',beta);
+assert(sum([hubs(3) hubs(7)] - [0.4669 0.4742])< 1e-3,'Hubs are incorrect');
+assert((authorities(4) - .6531)<1e-3,'Authorities are incorrect');
+assert(sum(eig_centrality([3:4 7]) - [.1886 .1886 .1886]')< 1e-3,'Eig Centrality is incorrect');
+
+[hubs authorities] =  rand_hits(A,struct('beta',1-beta));
+[h_pgrank] = pagerank(A, struct('beta',1-beta)); [a_pgrank] = pagerank(A',struct('beta',1-beta));
 centrality2centralization(hubs,'hub',1)
 centrality2centralization(authorities,'authority',1)
 centrality2centralization(h_pgrank,'pagerank',1)
 centrality2centralization(a_pgrank,'pagerank',1)
-assert(sum([hubs(3) hubs(7)] - [0.4669 0.4742])< 1e-3,'Hubs are incorrect');
-assert((authorities(4) - .6531)<1e-3,'Authorities are incorrect');
-assert(sum(eig_centrality([3:4 7]) - [.1886 .1886 .1886]')< 1e-3,'Eig Centrality is incorrect');
 clear;
 
 
 % White & Smyth Undirected.
-G = zeros(10,10)
+G = zeros(10,10);
 G(1,2) = 1; G(2,3) = 1; G(3,1) = 1;
 G(4,6) = 1; G(5,6) = 1; G(6,7) = 1;
 G(1,4) = 1; G(4,5) = 1; G(5,10) = 1; G(10,3) = 1;
 G(10,8) = 1; G(8,7) = 1; G(7,9) = 1; G(8,9) = 1;G(9,2) = 1;
 G = G+G';
 A = G;
-[hubs authorities eig_centrality] =  hits(A);
-[h_pgrank] = pagerank_centrality(A, beta); [a_pgrank] = pagerank_centrality(A',beta);
+eig_centrality = eigenvector_centrality_und(A);
 centrality2centralization(eig_centrality,'eigenvector',1);
-
-
-% Directed/Asymmetric
-G = zeros(10,10)
-G(1,3) = 1; G(2,1) = 1; G(2,3) = 1; G(3,10) = 1; G(10,5) = 1; G(5,4) = 1; G(4,1) = 1; G(4,6) = 1; G(6,7) = 1; G(9,2) = 1;
-G(5,6) = 1; %G(6,5) = 1;
-G(8,10) = 1;%G(10,8) = 1;
-G(7,8) = 1; %G(8,7) = 1;
-G(9,8) = 1; %G(8,9) = 1;
-G(9,7) = 1; %G(7,9) = 1;
-A = G;
-[hubs authorities eig_centrality] =  hits(A);
-[h_pgrank] = pagerank_centrality(A, beta);
-centrality2centralization(hubs,'hub',1)
-centrality2centralization(authorities,'authority',1)
-centrality2centralization(eig_centrality,'eigenvector',1)
-centrality2centralization(h_pgrank,'pagerank',1)
-
-
-%  White & Smyth, Directed.
-G = zeros(10,10)
-G(1,3) = 1; G(2,1) = 1; G(2,3) = 1; G(3,10) = 1;
-G(10,5) = 1; G(5,4) = 1; G(4,1) = 1; G(4,6) = 1; G(5,6) = 1; G(6,5) = 1;G(6,7) = 1;
-G(9,2) = 1;
-G(10,8) = 1; G(8,10) = 1;
-G(7,8) = 1; G(8,7) = 1;
-G(8,9) = 1; G(9,8) = 1;
-G(7,9) = 1; G(9,7) = 1;
-A = G;
-[hubs authorities eig_centrality] =  hits(A);
-[h_pgrank] = pagerank_centrality(A, beta);
-centrality2centralization(hubs,'hub',1)
-centrality2centralization(authorities,'authority',1)
-centrality2centralization(eig_centrality,'eigenvector',1)
-centrality2centralization(h_pgrank,'pagerank',1)
+disp('Should be equal for all nodes, with zero centralization');
